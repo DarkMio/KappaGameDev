d182e1c22a9de2534301a2e0f7169bfabec0f812
DarkMio <s59330@beuth-hochschule.de>
2016-06-10 20:54:10 +0200

    Adding like a billion things to the Node Graph
    Pre / Post Checks are in now, a lot of stuff runs smoother,.

---
+++ Assets/Trash/NodeInterface.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using NodeEditorFramework;
+using NodeEditorFramework.Utilities;
+
+[ExecuteInEditMode]
+public class NodeInterface : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		NodeCanvas x = NodeEditorFramework.NodeEditorSaveManager.LoadNodeCanvas("Assets/Plugins/Node_Editor/Resources/Saves/test_case.asset");
+		NodeEditor.RecalculateAll (x);
+		Debug.Log ("I may have loaded.");
+		foreach(Node n in x.nodes) {
+			
+			// n.Calculate ();
+			string s = "> " + n.name;
+			n.ClearCalculation ();
+			foreach (NodeInput ni in n.Inputs) {
+				if (ni.connection != null) {
+					s += " | INPUT: " + ni.connection.GetValue<float> ();
+				}
+			}
+
+			foreach (NodeOutput no in n.Outputs) {
+				s += " | OUTPUT: " + no.GetValue<float> ();
+			}
+
+			// Debug.Log (s);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("------------------------->");
+		Start ();
+	}
+}
