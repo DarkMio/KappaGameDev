f33f2d68d8d1c98d3ebba3c6d2cc6a11ad0964b0
DarkMio <s59330@beuth-hochschule.de>
2016-06-10 21:02:13 +0200

    I fucking lost it when suddenly dialoging was gone

--- Assets/Plugins/Node_Editor/Nodes/Dialogue/DialogueNode.cs
+++ Assets/Plugins/Node_Editor/Nodes/Dialogue/DialogueNode.cs
@@ -1,69 +1,160 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
 using NodeEditorFramework;
 using NodeEditorFramework.Utilities;
+using UnityEditor;
+using UnityEngine.UI;
 
-[Node (false, "Dialogue/Basic Dialogue")]
-public class DialogueNode : Node {
-	public const string ID = "dialogueNode";
-	public override string GetID{ get { return ID; } }
-	public override bool AllowRecursion { get { return true; } }
+namespace NodeEditorFramework.Standard {
+    [Node(false, "Dialogue/Basic Dialogue")]
+    public class DialogueNode : Node {
+        public const string ID = "dialogueNode";
 
-	private int extOptions = 1;
-	private int inOptions = 1;
-	
-	public override Node Create(Vector2 pos) {
-		DialogueNode node = CreateInstance<DialogueNode>();
-		node.rect = new Rect(pos.x, pos.y, 200, 100 + 50 * extOptions);
-		node.name = "Dialogue Node";
-		
-		node.CreateInput("Parent", "Boolean");
-		node.CreateOutput("Child", "Boolean");
-		return node;
-	}
-	
-	protected internal override void NodeGUI() {
-		int max = Mathf.Max(inOptions, extOptions);
-		rect.height = 63 + 18 * max;
-		
-		GUILayout.Label("Dialogue Node checking in!");
-		
-		for(int i = 0; i < max; i++) {
-			GUILayout.BeginHorizontal();
-			if(i < inOptions) {
-				GUILayout.BeginVertical();
-				Inputs[i].DisplayLayout();
-				GUILayout.EndVertical();
-			}
-			if(i < extOptions) {
-				GUILayout.BeginVertical();
-				Outputs[i].DisplayLayout();
-				GUILayout.EndVertical();
-			}
+        public override string GetID {
+            get { return ID; }
+        }
 
-			GUILayout.EndHorizontal();
-		}
-		GUILayout.BeginHorizontal();
-		GUILayout.BeginVertical();
-		if(GUILayout.Button("Add Parent")) {
-			inOptions++;
-			CreateInput("Parent", "Boolean");
-		}
+        public override bool AllowRecursion {
+            get { return true; }
+        }
 
-		GUILayout.EndVertical();
-		GUILayout.BeginVertical();
-		if(GUILayout.Button("Add Child")) {
-			extOptions++;
-			CreateOutput("Child", "Boolean");
-		}
-		GUILayout.EndVertical();
-		GUILayout.EndHorizontal();
-	}
-	
-	public override bool Calculate() {
-		if(!allInputsReady()) {
-			return false;
-		}
-		Outputs[0].SetValue<float>(Inputs[0].GetValue<float>() * 5);
-		return true;
-	}
+        private int extOptions = 1;
+        private int inOptions = 1;
+        private string _dialogueText = "Dialogue Main Text";
+        private List<string> _decisions = new List<string> {""};
+        private Vector2 scroller;
+        /*
+         * @TODO: This has currently no implied logic, pls add.
+         */
+        private List<AbstractCheckable> _preCheckables = new List<AbstractCheckable> {null};
+
+        public override Node Create(Vector2 pos) {
+            DialogueNode node = CreateInstance<DialogueNode>();
+            var max = Mathf.Max(inOptions, extOptions);
+            node.rect = new Rect(pos.x, pos.y, 300, 200 + 50*max);
+            node.name = "Dialogue Node";
+
+            node.CreateInput("Parent", "Boolean");
+            node.CreateOutput("Child", "Boolean");
+            return node;
+        }
+
+        protected internal override void NodeGUI() {
+            // super hard coded to get the node UI not completely broken.
+            // I figured that the NodeGUI wasn't planned to do fancy things, so this is with fixed sizes everywhere.
+            var max = Mathf.Max(inOptions, extOptions);
+            scroller = GUILayout.BeginScrollView(scroller);
+                // if we ever manage to break the UI again, then we have scrollbars.
+            rect.height = 200 + 50*max; // we can calculate a fixed node size with a fixed calculation
+            _dialogueText = GUILayout.TextArea(_dialogueText, GUILayout.Height(100)); // main dialogue
+
+            for (int i = 0; i < max; i++) {
+                // until max is reached we do...
+                
+                GUILayout.BeginHorizontal(); // new row to throw our pre check in
+                GUILayout.BeginVertical();
+                GUILayout.EndVertical();
+                if (i < extOptions) {
+                    _preCheckables[i] =
+                        (AbstractCheckable)
+                            EditorGUILayout.ObjectField(_preCheckables[i], typeof(AbstractCheckable), true);
+                } else {
+                    GUILayout.Label("");
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal(); // a horizontal
+                GUILayout.BeginVertical(); // display parent
+                if (i < inOptions) {
+                    Inputs[i].DisplayLayout();
+                }
+                else {
+                    GUILayout.Label("", GUILayout.Width(41)); // or an invisible label to not break the next coming box
+                }
+                GUILayout.EndVertical();
+
+                GUILayout.BeginVertical();
+                if (i < extOptions) {
+                    _decisions[i] = GUILayout.TextArea(_decisions[i], GUILayout.Height(30), GUILayout.Width(200));
+                        // a two line text box
+                }
+                else {
+                    GUILayout.Label("", GUILayout.Height(30)); // or just a magical label
+                }
+                GUILayout.EndVertical();
+                GUILayout.BeginVertical();
+                if (i < extOptions) {
+                    Outputs[i].DisplayLayout(); // output - can be missing to work well, too.
+                }
+                GUILayout.EndVertical();
+
+                GUILayout.EndHorizontal(); // end horizontal
+            }
+
+            GUILayout.FlexibleSpace(); // Fills out the bottom
+
+            // node option panel
+            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
+            if (GUILayout.Button("Add Parent")) {
+                inOptions++;
+                CreateInput("Parent", "Boolean");
+                EditorUtility.SetDirty(this);
+            }
+
+            GUILayout.EndVertical();
+            GUILayout.BeginVertical();
+            if (GUILayout.Button("Add Child")) {
+                extOptions++;
+                CreateOutput("Child", "Boolean");
+                _decisions.Add("");
+                _preCheckables.Add(null);
+                EditorUtility.SetDirty(this);
+            }
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
+            if (GUILayout.Button("Remove Parent")) {
+                inOptions--;
+                Inputs.RemoveAt(inOptions);
+                Inputs.TrimExcess();
+                EditorUtility.SetDirty(this);
+            }
+            GUILayout.EndVertical();
+            GUILayout.BeginVertical();
+            if (GUILayout.Button("Remove Child")) {
+                extOptions--;
+                Outputs.RemoveAt(extOptions);
+                Outputs.TrimExcess();
+                EditorUtility.SetDirty(this);
+            }
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
+            if (GUILayout.Button("Calculate")) {
+                Calculate();
+                EditorUtility.SetDirty(this);
+            }
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndScrollView();
+        }
+
+        public override bool Calculate() {
+            Debug.Log(_dialogueText);
+            if (!allInputsReady()) {
+                return false;
+            }
+            Outputs[0].SetValue<bool>(Inputs[0].GetValue<bool>());
+            return true;
+        }
+    }
 }
