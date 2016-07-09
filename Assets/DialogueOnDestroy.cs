10f86aaff88940954594f7c27be0f5b81cf0ebbe
DarkMio <s59330@beuth-hochschule.de>
2016-07-09 13:34:48 +0200

    Some atmospheric lights

--- Assets/DialogueOnDestroy.cs
+++ Assets/DialogueOnDestroy.cs
@@ -5,6 +5,6 @@ public class DialogueOnDestroy : MonoBehaviour {
 
 	void OnDestroy() {
 		GameObject go = GameObject.FindGameObjectWithTag("Player");
-		go.GetComponent<PlayerController>().enablePlayercontroller();
+		go.GetComponentInChildren<PlayerController>().enablePlayercontroller();
 	}
 }
---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+    private Light l;
+
+    public Vector2 flickerRange;
+
+
+	// Use this for initialization
+	void Start () {
+	    l = GetComponent<Light>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	    var val = Random.Range(flickerRange.x, flickerRange.y);
+	    var final = Vector2.MoveTowards(new Vector2(l.intensity, 0), new Vector2(val, 0), 0.01f);
+
+	    l.intensity = final.x;
+
+	}
+}
